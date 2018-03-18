52646673f6c5779c78ad1d35641d6f60b17caf56
nancynaan <37501524+nancynaan@users.noreply.github.com>
2018-03-18 18:10:50 +0200

    16.03

---
+++ homework-16.03.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// 2nd
+console.log("It's beautiful day. I like 'free day'.");
+console.log("It's beautiful day. I like \"free day\".");
+console.log('It\'s beautiful day. I like "free day".');
+console.log('It\'s beautiful day. I like \'free day\'.');
+console.log("It\u0027s beautiful day. I like \"free day\".");
+console.log("\n");
+
+// 3rd
+var firstNumber = 1.44*0.56*10000; //0.8064*10000
+var otherFirstNumber = 2.688*0.3*10000; //0.8064*10000
+var secondNumber = 3.14*2.03*10000; //6.3742*10000
+var otherSecondNumber = 3.1871*2*10000; //6.3742*10000
+var firstResult = (firstNumber*secondNumber)/10000;
+var otherSecondResult = (otherFirstNumber*otherSecondNumber)/10000;
+console.log(firstResult === otherSecondResult);
+console.log("\n");
+
+
+// 4th
+console.log("string" * 2);
+console.log("string" / 2);
+console.log("string" % 2);
