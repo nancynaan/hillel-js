a23e327324500a71253852ea119b8c46f82bc2f0
nancynaan <37501524+nancynaan@users.noreply.github.com>
2018-04-12 21:18:27 +0300

    add task 2
    add the second task - function returns the biggest value of students and corresponding year.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 let studentsData = {
     1: {
         name: 'Иванов Иван Иванович',
-        startDate: 1990,
-        endDate: 1995
+        startDate: 2000,
+        endDate: 2005
     },
     2: {
         name: 'Римусов Римус Римусович',
@@ -40,23 +40,66 @@ let studentsData = {
 
 //first
 let key;
-let students = '';
+let studentsString = '';
 function showStudents(startDate, endDate) {
     startDate = +prompt('Введите год поступления:');
     endDate = +prompt('Введите год окончания (отчисления):');
     for(key in studentsData) {
         if (studentsData[key].startDate==startDate && studentsData[key].endDate==endDate) {
-            students = students + studentsData[key].name + '\n';
+            studentsString = studentsString + studentsData[key].name + '\n';
         }
     }
-    return students;
+    return studentsString;
 }
 
-if (showStudents()== students) {
-    if (students === '') {
+if (showStudents()== studentsString) {
+    if (studentsString === '') {
         alert('Студентов по данному запросу не обнаружено!');
     } else {
-        alert(`В выбранный период обучались:\n${students}`);
+        alert(`В выбранный период обучались:\n${studentsString}`);
     }
 }
 
+//second
+let studentsObject = {};
+let sumOfStudents = 0;
+let keyValue;
+let maxValue = keyValue;
+let yearValue;
+
+function getPeriod(startDate, endDate) {
+	alert('Посмотрим в каком году училось наибольшее количество студентов!');
+    startDate = +prompt('Введите год начала периода:');
+    endDate = +prompt('Введите год окончания периода');
+    let key;
+    while (startDate<=endDate) {
+        for(key in studentsData) {
+            if(startDate>=studentsData[key].startDate && startDate<=studentsData[key].endDate){
+                sumOfStudents += 1;
+            }
+        }
+        studentsObject[startDate] = sumOfStudents;
+        startDate++;
+        sumOfStudents = 0;
+    }
+
+    // get a value of first key in studentsObject for the next comparasion of values for finding the biggest one
+    for(key in studentsObject) {
+        keyValue = studentsObject[key];
+        break;
+    }
+    // compare values of each key and find the biggest one
+    for(key in studentsObject) {
+        if (studentsObject[key] > keyValue && studentsObject[key] > studentsObject[key-1]) {
+            maxValue = studentsObject[key];
+            yearValue = key;
+        }
+    }
+    return (yearValue, maxValue);
+}
+
+if (getPeriod(yearValue, maxValue)) {
+    alert(`В ${yearValue} году училось наибольшее количество студентов - ${maxValue}.`);
+} else {
+    alert(`Вы ввели некорректное число! Повторите действия снова!`);
+}
